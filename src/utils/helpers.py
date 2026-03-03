ad8da0638be464e7342c2bdfd10eccb7a850a248
Nate Hunter <hunter.j.nate@gmail.com>
2026-03-03 09:58:51 -0500

    Added helper functions for managing assistants in OpenAI API

---
+++ src/utils/helpers.py
@@ -0,0 +1,36 @@
+from typing import TypedDict
+
+from openai import OpenAI
+from openai.types.beta.assistant import Assistant
+
+
+class AssistantProps(TypedDict):
+    name: str
+    instructions: str
+    model: str
+
+
+def get_or_create_assistant(client: OpenAI, props: AssistantProps) -> Assistant:
+    assistant = None
+    assistant_name = props["name"]
+    assistant_instructions = props["instructions"]
+    assistant_model = props["model"]
+    current_assistants = client.beta.assistants.list()
+    for asst in current_assistants:
+        if asst.name == assistant_name:
+            assistant = asst
+            break
+    if not assistant:
+        assistant = client.beta.assistants.create(
+            name=assistant_name,
+            instructions=assistant_instructions,
+            model=assistant_model,
+        )
+    return assistant
+
+
+def delete_assistants_by_name(client: OpenAI, assistant_name: str) -> None:
+    assistants = client.beta.assistants.list()
+    for asst in assistants:
+        if asst.name == assistant_name:
+            client.beta.assistants.delete(asst.id)
